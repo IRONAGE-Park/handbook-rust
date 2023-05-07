2088395769f0968090a671a6fb7e6ea2df1542ec
IRONAGE-Park <cheolhyundev@gmail.com>
2023-05-07 22:26:18 +0900

    # Chapter 3-5 Example Codes.

--- 3_5_branches/src/main.rs
+++ 3_5_branches/src/main.rs
@@ -4,6 +4,87 @@ fn main() {
     loop_label();
     while_loop();
     collection_loop();
+    let result = convert_c_f((32.0));
+    
+    let fibo = fibonacci(7);
+    
+    println!("result = {}, {fibo}", result);
+    
+    christmas_carol();
+}
+
+fn convert_f_c(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn convert_c_f(f: f64) -> f64 {
+    (f * 9.0 / 5.0) + 32.0
+}
+
+fn fibonacci(n: i32) -> i32 {
+    if n < 2 {
+        return n;
+    }
+    
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+fn christmas_carol() {
+    for day in 1..13 {
+        day_intro(day);
+        
+        for gift_day in (1..(day + 1)).rev() {
+            gift(
+                gift_day,
+                if gift_day == 1 && day != 1 {
+                    "and "
+                } else {
+                    ""
+                }
+            )
+        }
+    }
+}
+
+fn day_intro(n: u32) {
+    let day = match n {
+        1 => "first",
+        2 => "second",
+        3 => "third",
+        4 => "fourth",
+        5 => "fifth",
+        6 => "sixth",
+        7 => "seventh",
+        8 => "eighth",
+        9 => "ninth",
+        10 => "tenth",
+        11 => "eleventh",
+        12 => "twelfth",
+        _ => "",
+    };
+    
+    println!("On the {} day of Christmas", day);
+    println!("My true love gave to me");
+}
+
+fn gift(n: u32, prefix: &str) {
+    let gift_text = match n {
+        1 => "a partridge in a pear tree",
+        2 => "two turtle doves",
+        3 => "three French hens",
+        4 => "four calling birds",
+        5 => "five golden rings",
+        6 => "six geese a-laying",
+        7 => "seven swans a-swimming",
+        8 => "eight maids a-milking",
+        9 => "nine ladies dancing",
+        10 => "ten lords a-leaping",
+        11 => "eleven pipers piping",
+        12 => "twelve drummers drumming",
+        _ => "",
+    };
+    
+    println!("{prefix}{gift_text}");
 }
 
 fn condition() {
